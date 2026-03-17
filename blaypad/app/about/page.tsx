b061d78ce053c2c069314e276901b1ca07cfa8e1
Henrique Blay Barboza <trap02.mar@hotmail.com>
2026-03-17 00:09:34 -0300

    Just a line jump in about page
    No effect to the display

--- blaypad/app/about/page.tsx
+++ blaypad/app/about/page.tsx
@@ -7,7 +7,8 @@ export default function About() {
                 <main className="w-full max-w-3xl py-32 px-16 bg-white dark:bg-black">
                     <h1 className="text-3xl font-semibold text-black dark:text-zinc-50">About Blay Pad</h1>
                     <p className="mt-4 text-lg text-zinc-600 dark:text-zinc-400">
-                        Blay Pad is a Virtual Learning Environment designed to help learners of Deutsch. It provides a structured learning path with resources and community support to enhance your language learning experience.
+                        Blay Pad is a Virtual Learning Environment designed to help learners of Deutsch.
+                         It provides a structured learning path with resources and community support to enhance your language learning experience.
                     </p>
                 </main>
             </div>
